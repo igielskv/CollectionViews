5469d3bbd6895c246eb4306c3932f1e6d7cb415f
Manoli <igielskv@icloud.com>
2020-07-07 18:23:13 +0200

    Adds CollectionViewController and CollectionViewCell class to the Storyboard.

---
+++ CollectionViews/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Manoli on 07/07/2020.
+//  Copyright © 2020 macForce.one. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
